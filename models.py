229d3da4df8c949dbb23e955366c914546d9a4b5
Nanan232 <anamarihernandez23@gmail.com>
2025-04-13 13:13:56 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+    def check_password(self, password):
+        return self.password == password
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Teacher {self.name}>"
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
+    teacher = db.relationship('Teacher', backref='courses')
+
+    def __repr__(self):
+        return f"<Course {self.name}>"
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    classes = db.relationship('Class', secondary='enrollments', backref='students')
 
-class Class(db.Model):
+    def __repr__(self):
+        return f"<Student {self.name}>"
+
+class Enrollment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    grade = db.Column(db.String(5))
 
-# Association Table for Many-to-Many
-enrollments = db.Table('enrollments',
-    db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
-    db.Column('class_id', db.Integer, db.ForeignKey('class.id'))
-    )
+    student = db.relationship('Student', backref='enrollments')
+    course = db.relationship('Course', backref='enrollments')
+
+    def __repr__(self):
+        return f"<Enrollment {self.student.name} in {self.course.name}>"
